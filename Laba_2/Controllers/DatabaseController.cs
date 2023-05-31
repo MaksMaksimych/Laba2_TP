b5f7a2cef08f2e45cf7df46096860b52edb98c2c
MaksMaksimych <99993879+MaksMaksimych@users.noreply.github.com>
2023-05-31 22:38:49 +0300

    Add files via upload

---
+++ Laba_2/Controllers/DatabaseController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Laba_2.Models;
+using System.Web;
+using System.Reflection;
+using System.Net;
+
+
+namespace Laba_2.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // Список экземпляров Person
+        private static List<Person> _personsList = new List<Person>();
+
+        // Создание аккаунта
+        // GET: /Database/Creat_Data
+        [HttpGet]
+        public ActionResult Creat_Data()
+        {
+            HttpCookie cookie = Request.Cookies["ID"];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("ID");
+                cookie.Value = "0";
+            }
+            else
+            {
+                cookie.Value = (Convert.ToInt32(cookie.Value) + 1).ToString();
+            }
+            Response.Cookies.Add(cookie);
+            return View("Creat_Data");
+        }
+
+        // Просмотр созданного аккаунта 
+        [HttpPost]
+        public ActionResult View_Data(Person person)
+        {
+            if (Request.Cookies["ID"] != null)
+            {
+                string id = Request.Cookies["ID"].Value;
+                person.PersonId = Convert.ToInt32(id);
+                _personsList.Add(person);
+            }
+            return View("View_Data", person);
+        }
+
+        // Просмотр базы данных
+        public ActionResult Data_Base()
+        {
+            // true - внешний метод, false - внутренний;
+            ViewData["method"] = false; 
+            return View("Data_Base", _personsList);
+        }
+    }
+}
---
+++ Laba_2/Models/DatabaseModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Laba_2.Models
+{
+    public class Person
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PersonId { get; set; }
+
+        [Display(Name = "ФИО")]
+        public string Name { get; set; }
+
+        [Display(Name = "Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Рост")]
+        public uint Height { get; set; }
+
+        [Display(Name = "Телефон")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Наличие работы")]
+        public bool work { get; set; }
+    }
+}
